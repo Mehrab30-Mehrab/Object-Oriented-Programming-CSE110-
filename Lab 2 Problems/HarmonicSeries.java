f5a59fa509ce47147db9065b5287ebe13ddc1ee3
Mehrab Morshed Marjan <2025-1-60-142@std.ewubd.edu>
2026-02-12 01:38:11 +0600

    Add files via upload

---
+++ Lab 2 Problems/HarmonicSeries.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class HarmonicSeries {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = scanner.nextInt();
+        double sum = 0.0;
+        for (int i = 1; i <= n; i++) {
+            sum += 1.0 / i;
+        }
+        System.out.printf("Sum of the series is:%.2f ",sum);
+        scanner.close();
+    }
+}
